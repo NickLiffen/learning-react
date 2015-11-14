3d47d454c9e2096e6ec83caaf0a1dacde324d87d
Nick Liffen <UP653591@myport.ac.uk>
2015-11-14 22:17:41 +0000

    Version 1

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 
+
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
@@ -7,14 +8,13 @@ var app = express();
 
 var COMMENTS_FILE = path.join(__dirname, 'comments.json');
 
-
 app.set('port', (process.env.PORT || 3000));
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('api/comments', function(req, res) {
+app.get('/api/comments', function(req, res) {
   fs.readFile(COMMENTS_FILE, function(err, data) {
     if (err) {
       console.error(err);
@@ -25,13 +25,16 @@ app.get('api/comments', function(req, res) {
   });
 });
 
-app.post('api/comments', function(req, res) {
+app.post('/api/comments', function(req, res) {
   fs.readFile(COMMENTS_FILE, function(err, data) {
     if (err) {
       console.error(err);
       process.exit(1);
     }
     var comments = JSON.parse(data);
+    // NOTE: In a real implementation, we would likely rely on a database or
+    // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
+    // treat Date.now() as unique-enough for our purposes.
     var newComment = {
       id: Date.now(),
       author: req.body.author,
